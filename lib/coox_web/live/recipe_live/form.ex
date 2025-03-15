1dba16b0d7bdb977def79bac84a26e62f53fce63
Sven Van Caekenberghe <sven@beta9.be>
2025-03-15 11:36:45 +0100

    add phx-disable-with to button

--- lib/coox_web/live/recipe_live/form.ex
+++ lib/coox_web/live/recipe_live/form.ex
@@ -14,7 +14,7 @@ defmodule CooxWeb.RecipeLive.Form do
 
       <:actions>
         <div class="flex">
-          <.button>Save Recipe</.button>
+          <.button phx-disable-with="Saving...">Save Recipe</.button>
           <.loading_spinner class="hidden phx-submit-loading:inline-block ml-4 mb-5" />
         </div>
       </:actions>
