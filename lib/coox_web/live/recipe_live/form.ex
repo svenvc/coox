3708202f342e5c48402ede515b2a2b55f6b7de85
Sven Van Caekenberghe <sven@beta9.be>
2025-04-08 20:13:20 +0200

    add instruction inputs to form

--- lib/coox_web/live/recipe_live/form.ex
+++ lib/coox_web/live/recipe_live/form.ex
@@ -74,6 +74,20 @@ defmodule CooxWeb.RecipeLive.Form do
         phx-hook="MaintainHeight"
       />
 
+      <div class="mb-6">
+        <h2 class="text-lg font-semibold text-zinc-700">Instructions</h2>
+
+        <div id="instruction-inputs">
+          <.inputs_for :let={instruction_f} field={@form[:instructions]}>
+            <div class="flex items-center mt-4 mb-2 space-x-2">
+              <div class="grow">
+                <.input field={instruction_f[:description]} type="text" phx-debounce />
+              </div>
+            </div>
+          </.inputs_for>
+        </div>
+      </div>
+
       <:actions>
         <div class="flex">
           <.button phx-disable-with="Saving...">Save Recipe</.button>
